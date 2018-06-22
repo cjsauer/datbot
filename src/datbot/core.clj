cc65460889a9fdd5fa661272ccca81e41a2d1ddd
Calvin Sauer <calvin.j.sauer@gmail.com>
2018-06-22 11:21:07 -0400

    Add support for sending slack messages

--- src/datbot/core.clj
+++ src/datbot/core.clj
@@ -1,2 +1,21 @@
-(ns datbot.core)
+(ns datbot.core
+  (:require [clojure.java.io :as io]
+            [clj-http.client :as http]))
 
+(def config
+  (-> (io/resource "config.edn")
+      slurp
+      read-string))
+
+(defn send-message
+  [message]
+  (http/post (:slack-webhook-url config)
+             {:body (format "{\"text\": \"%s\"}" message)
+              :headers {"Content-Type" "application/json"}
+              :accept :json}))
+
+(comment
+
+  (send-message "Hello!")
+
+  )
